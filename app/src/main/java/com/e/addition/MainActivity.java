017ec7b1d493a8eef952bd4ddcb57f28bc9fb030
tidjanilawali <tidjani.issoufou@bluecrest.edu.gh>
2019-03-24 19:44:36 +0000

    Addition

---
+++ app/src/main/java/com/e/addition/MainActivity.java
@@ -0,0 +1,41 @@
+package com.e.addition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText num1;
+    private EditText num2;
+    private EditText result;
+    private Button add;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        num1 = (EditText) findViewById(R.id.etNum1);
+        num2 = (EditText) findViewById(R.id.etNum2);
+        result = (EditText) findViewById(R.id.tvAnswer);
+        add = (Button) findViewById(R.id.btnAdd);
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int number1 = Integer.parseInt(num1.getText().toString());
+                int number2 = Integer.parseInt(num2.getText().toString());
+                int sum = number1 + number2;
+                result.setText("Answer:" + String.valueOf(sum));
+            }
+
+        });
+    }
+
+}
+
